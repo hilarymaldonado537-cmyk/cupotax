cb1174d14b9a4fb73a9372dfce9ea669cef271db
hilarymaldonado537-cmyk <hilarymaldonado537@gmail.com>
2026-06-21 16:22:14 -0500

    CorsConfig.java

--- src/main/java/com/cupotax/config/CorsConfig.java
+++ src/main/java/com/cupotax/config/CorsConfig.java
@@ -6,10 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
